5353dba90982e1c3b4d3ab749c33513579782007
Stefanie Loh <stefkloh@gmail.com>
2018-06-20 11:10:59 -0700

    Added styling

--- react-memory-game/src/components/ProgressBar/ProgressBar.js
+++ react-memory-game/src/components/ProgressBar/ProgressBar.js
@@ -1,15 +1,24 @@
 import React from "react";
+import Container from "../../components/Container";
+import Row from "../../components/Row";
+import Col from "../../components/Col";
 
 const ProgressBar = props => (
-<nav className="navbar navbar-expand-lg navbar-expand-sm navbar-expand-xs navbar-light bg-light sticky-top">
-  <a className="navbar-brand" href="/">Clicky Game</a>
-  <div>
-      {props.progressReport}
-  </div>
-  <div>
-    Score: {props.currentScore} | Top Score: {props.topScore}
-  </div>
-</nav>
+  <nav className="navbar navbar-expand-lg navbar-expand-sm navbar-expand-xs navbar-light bg-light sticky-top">
+    <Container>
+      <Row>
+        <Col size="md-4">
+          <a className="navbar-brand" href="/">Clicky Game</a>
+        </Col>
+        <Col size="md-4">
+            {props.progressReport}
+        </Col>
+        <Col size="md-4">
+          Score: {props.currentScore} | Top Score: {props.topScore}
+        </Col>
+      </Row>
+    </Container>
+  </nav>
 );
 
 export default ProgressBar;
